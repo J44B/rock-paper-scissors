8c34e22126df64d5755ab9ed05377aba9b56ec1a
jochen <jochen@byte23.net>
2024-09-27 16:12:10 +0200

    added javascript-file

---
+++ rps.js
@@ -0,0 +1,6 @@
+// Create a game of Rock Paper Scissors to play in the console against the computer
+// git@github.com:J44B/rock-paper-scissors.git
+
+console.info(
+    "Welcome to a game of Rock - Paper - Scissors. Please choose one of the three options, then hope for the best."
+);

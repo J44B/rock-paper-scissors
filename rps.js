442898c4ca8d3835697cd4940c216fbd886423f5
jochen <jochen@byte23.net>
2024-10-04 08:19:42 +0200

    correction to winner decision logic

--- rps.js
+++ rps.js
@@ -2,35 +2,33 @@
 // git@github.com:J44B/rock-paper-scissors.git
 
 // define possible options
-const options = ["Rock", "Paper", "Scissors"];
+const options = ['Rock', 'Paper', 'Scissors'];
 
 // Get user input and slice array
 
-const userChoice = process.argv.slice(2);
-
+const userChoice = process.argv[2];
+console.log(userChoice);
+console.log(process.argv);
 // get random option for computer
 
-const computerChoice = [Math.floor(Math.random() * options.length)];
+const computerChoice = options[Math.floor(Math.random() * options.length)];
 
 // Define who is the winner
 const winner = function whoWins() {
-    // Check for draw
-    if (computerChoice === userChoice) {
-        return "It's a draw.";
-        // Check for winner
-    } else if (
-        (userChoice === "Rock" && computerChoice === "Scissors") ||
-        (userChoice === "Paper" && computerChoice === "Rock") ||
-        (userChoice === "Scissors" && computerChoice === "Paper")
-    ) {
-        return "You win. Congratulations.";
-    }
-    return "I win. Haha!";
+  // Check for draw
+  if (computerChoice === userChoice) {
+    return "It's a draw.";
+    // Check for winner
+  } else if (
+    (userChoice === 'Rock' && computerChoice === 'Scissors') ||
+    (userChoice === 'Paper' && computerChoice === 'Rock') ||
+    (userChoice === 'Scissors' && computerChoice === 'Paper')
+  ) {
+    return 'You chose wisely. Congratulations.';
+  } else {
+    return 'You chose poorly. Haha!';
+  }
 };
 
 // Final output
-console.log(
-    `You chose ${userChoice}. I chose ${
-        (computerChoice, options[computerChoice])
-    }. ${winner()}`
-);
+console.log(`You chose ${userChoice}. I chose ${computerChoice}. ${winner()}`);
